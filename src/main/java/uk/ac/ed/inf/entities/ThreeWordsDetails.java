55810e0ab0b3f6ddb23f0697692b05e30417ce29
nkadiadrian <adriannkadi@gmail.com>
2021-12-01 18:57:50 +0100

    -Add ThreeWordsDetails file

---
+++ src/main/java/uk/ac/ed/inf/entities/ThreeWordsDetails.java
@@ -0,0 +1,17 @@
+package uk.ac.ed.inf.entities;
+
+import com.google.gson.annotations.SerializedName;
+import uk.ac.ed.inf.LongLat;
+
+public class ThreeWordsDetails {
+    @SerializedName("coordinates")
+    private LongLat longLat;
+
+    public ThreeWordsDetails(LongLat longLat) {
+        this.longLat = longLat;
+    }
+
+    public LongLat getLongLat() {
+        return longLat;
+    }
+}
